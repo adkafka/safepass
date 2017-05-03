abffcdb6d784fb48d68f601ade7672e60657b94f
Adam Kafka <adkafka@gmail.com>
2017-05-03 16:39:44 -0400

    Added serializer/encrypter (and inverse). Started on interface as well.
    Next step will be making the 'database' object and its functionality

---
+++ DillGPG.py
@@ -0,0 +1,51 @@
+import gnupg
+import dill
+import sys
+import getpass
+
+
+class DillGPG:
+    def __init__(self,gpghome='~/.gnupg',algo='AES256'):
+        self.gpg = gnupg.GPG(gnupghome=gpghome,use_agent=True)
+        self.algo = algo
+
+    def get_password(self):
+        return getpass.getpass(prompt='Enter password:')
+
+    def save(self, obj, filename):
+        passwd = self.get_password()
+        serialized_string = dill.dumps(obj)
+        encrypted_data = self.gpg.encrypt(serialized_string,None,
+                symmetric=self.algo,passphrase=passwd)
+        if encrypted_data.ok != True:
+            print("Error saving encrypted data:")
+            print(encrypted_data.status)
+            sys.exit(1)
+        else:
+            with open(filename,"w") as file_out:
+                file_out.write(encrypted_data.data)
+
+    def open(self, filename):
+        passwd = self.get_password()
+        with open(filename,"r") as file_in:
+            decrypted_data = self.gpg.decrypt(file_in.read(),passphrase=passwd)
+            if decrypted_data.ok != True:
+                print("Error opening encrypted data:")
+                print(decrypted_data.status)
+                sys.exit(1)
+            else:
+                obj = dill.loads(decrypted_data.data)
+                return obj
+
+
+def test():
+    dgpg = DillGPG()
+    obj = {"test": 5, "blah": [1,3,5]}
+    dgpg.save(obj,"test")
+    obj2 = dgpg.open("test")
+    print(obj2)
+    
+
+
+if __name__ == '__main__':
+    test()
